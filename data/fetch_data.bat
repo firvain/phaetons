410050de57033873c6f87f4f1b552264df458e1c
firvain <etsipis@terracognita.gr>
2020-10-05 14:34:52 +0300

    fix path for fetch data log

--- data/fetch_data.bat
+++ data/fetch_data.bat
@@ -1,4 +1,4 @@
-SET logfile="C:\Users\Tsipis\Documents\data\run.log"
+SET logfile="C:\Users\Tsipis\phaetons\data\run.log"
 @echo off
 @echo Starting Script at %date% %time% >> %logfile%
 %windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -Command "& 'C:\Users\Tsipis\anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\Tsipis\anaconda3\envs\phaetons_37' ; python C:\Users\Tsipis\phaetons\data\utils\data_demokritos.py -t 24h; python C:\Users\Tsipis\phaetons\data\utils\data_weather_predictions.py ; conda deactivate"
